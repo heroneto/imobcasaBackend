5eb9e88324e1d00a170b1d17e8a67816b2f92751
Heron <hhqe@hotmail.com>
2020-05-21 18:46:43 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req,res) =>{
+  res.send('Hello world')
+})
+
+app.post('/rest/api/lead', (req,res) =>{
+  console.log(req.body)
+  res.send("Tnks")
+})
+
+app.listen(port, () => {
+  console.log(`Servidor executando na porta ${port}`)
+})
