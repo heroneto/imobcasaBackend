c0302ab82ce0cff74915af19dee4c22f28f0be65
Heron <hhqe@hotmail.com>
2020-05-21 19:00:17 -0300

    fix: fixed to use dotenv package

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+require('dotenv').config({path: path.resolve(__dirname, './.env')})
 const port = process.env.PORT || 3000
 
 app.use(express.json())
