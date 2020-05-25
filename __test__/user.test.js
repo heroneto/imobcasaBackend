4ad9e55b4405e0ebd00fc1350c589b29743edc1b
Heron <hhqe@hotmail.com>
2020-05-25 12:53:09 -0300

    test: user:get tests

---
+++ __test__/user.test.js
@@ -0,0 +1,143 @@
+const request = require('supertest')
+const server = require('../setup/server')
+const app = server()
+
+
+describe('GET:User routes tests', () =>{
+  it('Should return 403 if no token was provided', async () =>{
+    const res = await request(app)
+      .get('/user')
+    expect(res.status).toEqual(403)
+    expect(res.text).toBe('MissingParamError: token')
+  })
+  it('Should return 403 if invalid token was provided', async () => {
+    const res = await request(app)
+      .get('/user')
+      .query({
+        token: 'invalidToken'
+      })
+    expect(res.status).toEqual(403)
+    expect(res.text).toBe('InvalidParamError')
+  })
+  it('Shoud return 200 if valid request was send', async () => {
+    const res = await request(app)
+      .get('/user')
+      .query({
+        token: 'validToken'
+      })
+    expect(res.status).toEqual(200)
+    expect(res.body).toHaveProperty('users')
+  })
+})
+
+
+// describe('POST:User routes tests', () =>{
+//   it('Should return 403 if no token was provided', async () =>{
+//     const res = await request(app)
+//       .get('/user')
+//     expect(res.status).toEqual(403)
+//     expect(res.text).toBe('MissingParamError: token')
+//   })
+//   it('Should return 403 if invalid token was provided', async () => {
+//     const res = await request(app)
+//       .get('/user')
+//       .query({
+//         token: 'invalidToken'
+//       })
+//     expect(res.status).toEqual(403)
+//     expect(res.text).toBe('InvalidParamError')
+//   })
+//   it('Should return 400 if no username was provided', async () => {
+//     const res = await request(app)
+//       .post('/user')
+//       .query({
+//         fullName: 'validFullName',
+//         // username: 'validUsername',
+//         email: 'validEmail',
+//         password: 'validPassword',
+//         passwordConfirmation: 'validPassowdConfirmation',
+//         manager: true
+//       })
+//     expect(res.status).toEqual(403)
+//     expect(res.text).toBe('MissingParamError')
+//   })
+//   it('Should return 400 if no fullName was provided', async () => {
+//     const res = await request(app)
+//       .post('/user')
+//       .query({
+//         // fullName: 'validFullName',
+//         username: 'validUsername',
+//         email: 'validEmail',
+//         password: 'validPassword',
+//         passwordConfirmation: 'validPassowdConfirmation',
+//         manager: true
+//       })
+//     expect(res.status).toEqual(403)
+//     expect(res.text).toBe('MissingParamError: fullName')
+//   })
+  // it('Should return 400 if no email was provided', async () => {
+  //   const res = await request(app)
+  //     .post('/user')
+  //     .query({
+  //       fullName: 'validFullName',
+  //       username: 'validUsername',
+  //       // email: 'validEmail',
+  //       password: 'validPassword',
+  //       passwordConfirmation: 'validPassowdConfirmation',
+  //       manager: true
+  //     })
+  //   expect(res.status).toEqual(403)
+  //   expect(res.text).toBe('MissingParamError')
+  // })
+  // it('Should return 400 if no password was provided', async () => {
+  //   const res = await request(app)
+  //     .post('/user')
+  //     .query({
+  //       fullName: 'validFullName',
+  //       username: 'validUsername',
+  //       email: 'validEmail',
+  //       // password: 'validPassword',
+  //       passwordConfirmation: 'validPassowdConfirmation',
+  //       manager: true
+  //     })
+  //   expect(res.status).toEqual(403)
+  //   expect(res.text).toBe('MissingParamError')
+  // })
+  // it('Should return 400 if no passwordConfirmation was provided', async () => {
+  //   const res = await request(app)
+  //     .post('/user')
+  //     .query({
+  //       fullName: 'validFullName',
+  //       username: 'validUsername',
+  //       email: 'validEmail',
+  //       password: 'validPassword',
+  //       // passwordConfirmation: 'validPassowdConfirmation',
+  //       manager: true
+  //     })
+  //   expect(res.status).toEqual(403)
+  //   expect(res.text).toBe('MissingParamError')
+  // })
+  // it('Should return 400 if no manager rule was provided', async () => {
+  //   const res = await request(app)
+  //     .post('/user')
+  //     .query({
+  //       fullName: 'validFullName',
+  //       username: 'validUsername',
+  //       email: 'validEmail',
+  //       password: 'validPassword',
+  //       passwordConfirmation: 'validPassowdConfirmation',
+  //       // manager: true
+  //     })
+  //   expect(res.status).toEqual(403)
+  //   expect(res.text).toBe('MissingParamError')
+  // })
+  // it('Should return 200 if valid request was send', async () => {
+  //   const res = await request(app)
+  //     .get('/user')
+  //     .query({
+  //       token: 'validToken'
+  //     })
+  //   expect(res.status).toEqual(200)
+  //   expect(res.body).toHaveProperty('users')
+  // })
+// })
