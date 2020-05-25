9c2ebd1101f0803428069046607f37c6ca4571ee
Heron <hhqe@hotmail.com>
2020-05-25 18:44:13 -0300

    feat: start database

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const server = require('./setup/server')
-
-
+const startDatabase = require('./setup/database')
+const path = require('path')
+require('dotenv').config({path: path.resolve(__dirname,'.env')})
 
 async function startApp(){
+  await startDatabase(process.env.NODE_ENV || 'development')
   await server()
 }
 
