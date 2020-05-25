05a830c90e5f6be1601528fc964c027e7637c2fd
Heron <hhqe@hotmail.com>
2020-05-25 12:50:35 -0300

    refactor: moved server to setup folder

---
+++ setup/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+require('dotenv').config({path: path.resolve(__dirname, '../.env')})
+const port = process.env.PORT || 3000
+const routes = require('../src/routes')
+
+
+const startServer = () => {
+  app.use(express.json())
+  app.use(express.urlencoded({extended: true}))
+  app.use(routes)
+  
+  app.listen(port, () => {
+    console.log(`Servidor executando na porta ${port}`)
+  })
+  return app
+}
+
+
+
+module.exports = startServer
