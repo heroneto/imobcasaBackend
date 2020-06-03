64f2c29610b17cfebd6afa1baae30629ec894d89
Heron <hhqe@hotmail.com>
2020-06-02 22:15:40 -0300

    feat: create dev user in database if envirnment is development

--- setup/database.js
+++ setup/database.js
@@ -1,11 +1,32 @@
 const Sequelize = require('sequelize')
 const config = require('../config/database')
 const models = require('../src/models')
+const path = require('path')
+require('dotenv').config({path: path.resolve(__dirname, '../.env')})
+const env = process.env.NODE_ENV || 'development'
+
+
+const devUser = {
+  username: "Vagner",
+	fullName: "Vagner Zanella",
+	email: "vagner@email.com",
+	password: "123Mudar",
+	manager: true
+}
 
 const startDatabase = async (env) => {
   try{
     const sequelize = new Sequelize(config[env])
     await models.sequelize.sync({force: true})
+
+    if(env === 'development'){
+      const User = require('../src/models/').User
+      const user = await User.findOrCreate({
+        where: {username: devUser.username},
+        defaults: devUser
+      })
+    }
+
   }catch(err){
     console.log(err)
   }
