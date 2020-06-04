9e0a76569b4597606e2392efc7a7c91b106528ee
Heron <hhqe@hotmail.com>
2020-06-03 22:01:54 -0300

    test: return 400 if no email has beem send

--- __test__/userControler.test.js
+++ __test__/userControler.test.js
@@ -54,4 +54,13 @@ describe('USER CONTROLLER: tests', async () =>{
     expect(res.status).toHaveBeenCalledWith(400)
     expect(res.send).toBeCalledWith('MissingParamError: fullName')
   })
+  it('POST: Should return 400 if no email has beem send', async()=>{
+    const user = mockFakeUser()
+    delete user.email
+    const res = mockResponse()
+    const req = mockRequest(user)
+    await createUser(req, res)
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.send).toBeCalledWith('MissingParamError: email')
+  })
 })
