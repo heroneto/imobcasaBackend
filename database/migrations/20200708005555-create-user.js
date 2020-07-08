b39ef8c604a9cef5e8c62f8f209a9006384ecaea
Heron <hhqe@hotmail.com>
2020-07-07 22:52:23 -0300

    refactor: created user and leads model with migrations

---
+++ database/migrations/20200708005555-create-user.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('User', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fullName: {
+        type: Sequelize.STRING,
+        allowNull:false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull:false,
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      admin: {
+        type: Sequelize.BOOLEAN,
+        default: false        
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('User');
+  }
+};
