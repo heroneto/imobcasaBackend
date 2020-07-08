e3d0b4b38e22a9b324a4d0a69ac61a874a21fd98
Heron <hhqe@hotmail.com>
2020-07-07 22:53:58 -0300

    feat: execute all pending migrations instead sequelize.sync

--- setup/database.js
+++ setup/database.js
@@ -3,28 +3,40 @@ const config = require('../config/database')
 const models = require('../src/models')
 const path = require('path')
 require('dotenv').config({path: path.resolve(__dirname, '../.env')})
-const env = process.env.NODE_ENV || 'development'
-
+const Umzug = require('umzug')
+const env = process.env.NODE_ENV.trim()
 
 const devUser = {
   username: "admin",
 	fullName: "Administrador",
-	email: "admin@email.com",
 	password: "admin",
-	manager: true
+	admin: true
 }
 
-const startDatabase = async (env) => {
+const startDatabase = async () => {
   try{
     const sequelize = new Sequelize(config[env])
-    await models.sequelize.sync({force: true})
-
+    const umzug = new Umzug({
+      migrations: {
+        path: path.join(__dirname, '../database/migrations'),
+        params: [
+          sequelize.getQueryInterface(),
+          Sequelize
+        ]
+      },
+      storage: 'sequelize',
+      storageOptions: {
+        sequelize: sequelize
+      }
+    })
+    await umzug.up()
+    
     if(env === 'development'){
-      const User = require('../src/models/').User
-      const user = await User.findOrCreate({
-        where: {username: devUser.username},
-        defaults: devUser
-      })
+        const User = models.User
+        await User.findOrCreate({
+          where: {username: devUser.username},
+          defaults: devUser
+        })
     }
 
   }catch(err){
