c1651f4bd2655e4afc9fa8e1ffa1150f23138054
Heron <hhqe@hotmail.com>
2020-07-07 22:50:14 -0300

    feat: enabled logs in development enviroment

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": "0",
-    "logging": false
+    "logging": true
   },
   "test": {
     "username": "root",
