c45855f49e6ce7aa16f2aa62f16a6e38aa2a9f73
Heron <hhqe@hotmail.com>
2020-07-08 22:36:36 -0300

    test: Should return 400 is invalid id has been send

--- src/controllers/leadController.test.js
+++ src/controllers/leadController.test.js
@@ -1,4 +1,4 @@
-const {createLead} = require('./leadController')
+const {getLead} = require('./leadController')
 const { invalidParamError, missingParamError } = require('../Errors/')
 
 const mockResponse = () => {
@@ -19,9 +19,17 @@ describe('LEAD CONTROLLER: tests', async () => {
   it('GET: Should return 400 if no id has been send', async () => {
     const res = mockResponse()
     const req = mockRequest({})
-    await createLead(req, res)
+    await getLead(req, res)
     expect(res.status).toHaveBeenCalledWith(400)
     const { error } = missingParamError("id")
+    expect(res.send).toBeCalledWith(error)
+  })
+  it('GET: Should return 400 is invalid id has been send', async () => {
+    const res = mockResponse()
+    const req = mockRequest({id: "InvalidId"})
+    await getLead(req, res)
+    expect(res.status).toBeCalledWith(400)
+    const { error } = invalidParamError('id')
     expect(res.send).toBeCalledWith(error)
   })
 })
