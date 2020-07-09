455a76ae63150768f423e25534a5778fbb0b6f14
Heron <hhqe@hotmail.com>
2020-07-08 23:04:28 -0300

    test:  Should return 200 if user has been found

--- src/controllers/leadController.test.js
+++ src/controllers/leadController.test.js
@@ -1,5 +1,16 @@
 const {getLead} = require('./leadController')
 const { invalidParamError, missingParamError } = require('../Errors/')
+const {Leads} = require('../models/')
+const startDatabase = require('../../setup/database')
+
+const mockFakeLead = () => {
+  const fakeLead = {
+    name: "validLead",
+    phone: "999123491234",
+    source: "validSource",
+  }
+  return fakeLead
+}
 
 const mockResponse = () => {
   const res = {};
@@ -15,7 +26,19 @@ const mockRequest = (body) => {
   return request
 }
 
-describe('LEAD CONTROLLER: tests', async () => {
+beforeAll(async () => {
+  try{
+    startDatabase()
+    const fakeLead = mockFakeLead()
+    await Leads.create(fakeLead)
+  }catch(err){
+    console.log(err.toString())
+  }
+})
+
+
+
+describe('LEAD CONTROLLER: tests', () => {
   it('GET: Should return 400 if no id has been send', async () => {
     const res = mockResponse()
     const req = mockRequest({})
@@ -32,4 +55,16 @@ describe('LEAD CONTROLLER: tests', async () => {
     const { error } = invalidParamError('id')
     expect(res.send).toBeCalledWith(error)
   })
+  it('GET: Should return 200', async () => {
+    const res = mockResponse()
+    const req = mockRequest({id: 1})
+    await getLead(req, res)
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.send).toBeCalledWith(expect.objectContaining({
+      name: expect.any(String),
+      phone: expect.any(String),
+      source: expect.any(String)
+    }))
+  })
+
 })
