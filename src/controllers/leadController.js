f02033069b72e3e6da576aa0e52c610a2af14db5
Heron <hhqe@hotmail.com>
2020-07-08 22:37:38 -0300

    feat: Should return 400 is invalid id has been send

--- src/controllers/leadController.js
+++ src/controllers/leadController.js
@@ -1,9 +1,9 @@
 const {forbiden, invalidRequest, unauthorized, internalError} = require('../Protocols/httpCodes')
 const {invalidParamError, missingParamError, serverError} = require('../Errors/')
-
+const { Leads } = require('../models/')
 
 module.exports = {
-  createLead: async (req,res) => {
+  getLead: async (req,res) => {
     try{
       const {id} = req.body
       if(!id){
@@ -11,9 +11,24 @@ module.exports = {
         const {statusCode, body} = invalidRequest(error)
         return res.status(statusCode).send(body)
       }
-      return res.status(200).send('ok')
+      const lead = await Leads.findOne({
+        where: {
+          id: id
+        }
+      })
+      
+      if(!lead){
+        const {error} = invalidParamError('id')
+        const {statusCode, body} = invalidRequest(error)
+        return res.status(statusCode).send(body)
+      }
+      
+      return res.status(200).send(lead)
     }catch(err){
       console.log(err)
+      const {error} = serverError()
+      const {statusCode, body} = internalError(error)
+      return res.status(statusCode).send(body)
     }
   }
 }
