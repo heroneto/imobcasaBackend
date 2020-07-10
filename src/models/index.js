a4080206d001d7bccc016838fa17a73afeee0349
Heron <hhqe@hotmail.com>
2020-07-09 21:33:09 -0300

    refactor: require Sequelize from sequelize lib

--- src/models/index.js
+++ src/models/index.js
@@ -2,11 +2,11 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
+const {Sequelize} = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development'
 const config = require(__dirname + '/../../config/database.js')[env.trim()];
-const db = {};
+const db = {}
 
 let sequelize;
 if (config.use_env_variable) {
@@ -15,13 +15,20 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+// const db = {
+//   Sequelize: Sequelize,
+//   sequelize,
+//   User: sequelize.import('./user'),
+//   Lead: sequelize.import('./leads')
+// };
+
 fs
   .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
