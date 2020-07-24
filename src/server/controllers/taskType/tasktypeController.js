edabddf493b177bf6135a1207d03fcf7ed8d5f15
Heron Eto <41599309+heroneto@users.noreply.github.com>
2020-07-23 21:55:32 -0300

    feat: tasktype controller GET and POST functions

--- src/server/controllers/taskType/tasktypeController.js
+++ src/server/controllers/taskType/tasktypeController.js
@@ -1,22 +1,56 @@
-const tasktype = require('../../models').tasktype
+const tasktypeModel = require('../../models').tasktype
 const { invalidParamError, missingParamError, missingBodyContent } = require('../config').errors
 const { internalError, invalidRequest } = require('../config').protocols
 
 module.exports = {
-    createTask: (req,res) => {
-        try{
-            const requiredFields = ['name', 'description', 'active']
-            for(const field of requiredFields){
-                if(!req.body[`${field}`]){
-                  const {error} = missingParamError(field)
-                  const {statusCode, body} = invalidRequest(error)
-                  return res.status(statusCode).send(body)
-                }
+    createTaskType: async (req,res) => {
+      try{
+        const requiredFields = ['name', 'description', 'active']
+        for(const field of requiredFields){
+            if(!req.body[`${field}`]){
+              const {error} = missingParamError(field)
+              const {statusCode, body} = invalidRequest(error)
+              return res.status(statusCode).send(body)
             }
-            return res.status(200).send('ok')
-        }catch(err){
-            const { statusCode, body } = internalError(err)
-            return res.status(statusCode).send(body)
         }
+        const tasktype = await tasktypeModel.create(req.body)
+        return res.status(200).send(tasktype)
+      }catch(err){
+          const { statusCode, body } = internalError(err)
+          return res.status(statusCode).send(body)
+      }
+    },
+    getTaskType: async (req,res) => {
+      try{
+        const {id} = req.query
+        if(!id){
+          const {error} = missingParamError('id')
+          const {statusCode, body} = invalidRequest(error)
+          return res.status(statusCode).send(body)
+        }
+        const tasktype = await tasktypeModel.findOne({where: {
+          id: id
+        }})
+        return res.status(200).send(tasktype)
+      }catch(err){
+        console.log(err)
+        const { statusCode, body } = internalError(err)
+        return res.status(statusCode).send(body)
+      }
+    },
+    updateTaskType: async (req,res) => {
+      try{
+        const { id } = req.query
+        if(!id){
+          const {error} = missingParamError('id')
+          const {statusCode, body} = invalidRequest(error)
+          return res.status(statusCode).send(body)
+        }
+        return res.status(200).send('ok')
+      }catch(err){
+        console.log(err)
+        const { statusCode, body } = internalError(err)
+        return res.status(statusCode).send(body)
+      }
     }
 }
