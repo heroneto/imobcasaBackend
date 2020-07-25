8c16a770cc1eb192f9060ca46c7fcbe3e85bb6d7
Heron Eto <41599309+heroneto@users.noreply.github.com>
2020-07-24 22:36:48 -0300

    feat: Should return 400 if invalid id has been send

--- src/server/controllers/tasks/taskController.js
+++ src/server/controllers/tasks/taskController.js
@@ -74,6 +74,12 @@ module.exports = {
             return res.status(statusCode).send(body)
           }
       }
+      const task = await Tasks.findOne({where: {id: id}})
+      if(!task){
+        const {error} = invalidParamError('id')
+        const {statusCode, body} = invalidRequest(error)
+        return res.status(statusCode).send(body)
+      }
       return res.status(200).send('ok')
     }catch(err){
       console.log(err)
