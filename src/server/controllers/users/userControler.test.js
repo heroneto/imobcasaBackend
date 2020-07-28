b96872ccb063a2f228b42c28ba18665c7240ee47
Heron Eto <41599309+heroneto@users.noreply.github.com>
2020-07-28 18:46:44 -0300

    refactor: moved required fields tests to loop execution

--- src/server/controllers/users/userControler.test.js
+++ src/server/controllers/users/userControler.test.js
@@ -36,68 +36,38 @@ beforeAll(async () => {
 })
 
 describe('USER CONTROLLER: tests', () =>{
-  it('POST: Should return 400 if no Username has beem send', async() =>{
-    const user = mockFakeUser()
-    delete user.username
-    const res = mockResponse()
-    const req = mockRequest(user)
-    await createUser(req, res)
-    expect(res.status).toHaveBeenCalledWith(400)
-    expect(res.send).toBeCalledWith('MissingParamError: username')
+  describe('POST User tests', () => {
+    const requiredFields = ['fullName', 'username', 'email', 'password', 'admin']
+    for(const field of requiredFields){
+      it(`POST: Should return 400 if no ${field} has beem send`, async() =>{
+        const user = mockFakeUser()
+        delete user[`${field}`]
+        const res = mockResponse()
+        const req = mockRequest(user)
+        await createUser(req, res)
+        expect(res.status).toHaveBeenCalledWith(400)
+        expect(res.send).toBeCalledWith(`MissingParamError: ${field}`)
+      })
+    }
+    it('POST: Should return 200 if user has been created', async () =>{
+      const user = mockFakeUser()
+      const res = mockResponse()
+      const req = mockRequest(user)
+      await createUser(req,res)
+      expect(res.status).toHaveBeenCalledWith(200)
+      expect(res.json).toHaveBeenCalledWith({user:expect.objectContaining({
+        createdAt: expect.any(Date),
+        email: expect.any(String),
+        fullName: expect.any(String),
+        id: expect.any(Number),
+        admin: expect.any(Boolean),
+        password: expect.any(String),
+        updatedAt: expect.any(Date),
+        username: expect.any(String),
+      })})
+    })
   })
-  it('POST: Should return 400 if no fullName has beem send', async()=>{
-    const user = mockFakeUser()
-    delete user.fullName
-    const res = mockResponse()
-    const req = mockRequest(user)
-    await createUser(req, res)
-    expect(res.status).toHaveBeenCalledWith(400)
-    expect(res.send).toBeCalledWith('MissingParamError: fullName')
-  })
-  it('POST: Should return 400 if no email has beem send', async()=>{
-    const user = mockFakeUser()
-    delete user.email
-    const res = mockResponse()
-    const req = mockRequest(user)
-    await createUser(req, res)
-    expect(res.status).toHaveBeenCalledWith(400)
-    expect(res.send).toBeCalledWith('MissingParamError: email')
-  })
-  it('POST: Should return 400 if no password has beem send', async()=>{
-    const user = mockFakeUser()
-    delete user.password
-    const res = mockResponse()
-    const req = mockRequest(user)
-    await createUser(req, res)
-    expect(res.status).toHaveBeenCalledWith(400)
-    expect(res.send).toBeCalledWith('MissingParamError: password')
-  })
-  it('POST: Should return 400 if no admin attribute has beem send', async()=>{
-    const user = mockFakeUser()
-    delete user.admin
-    const res = mockResponse()
-    const req = mockRequest(user)
-    await createUser(req, res)
-    expect(res.status).toHaveBeenCalledWith(400)
-    expect(res.send).toBeCalledWith('MissingParamError: admin')
-  })
-  it('POST: Should return 200 if user has been created', async () =>{
-    const user = mockFakeUser()
-    const res = mockResponse()
-    const req = mockRequest(user)
-    await createUser(req,res)
-    expect(res.status).toHaveBeenCalledWith(200)
-    expect(res.json).toHaveBeenCalledWith({user:expect.objectContaining({
-      createdAt: expect.any(Date),
-      email: expect.any(String),
-      fullName: expect.any(String),
-      id: expect.any(Number),
-      admin: expect.any(Boolean),
-      password: expect.any(String),
-      updatedAt: expect.any(Date),
-      username: expect.any(String),
-    })})
-  })
+  
   it('GET: Should return 200', async () =>{
     const res = mockResponse()
     const req = mockRequest()
