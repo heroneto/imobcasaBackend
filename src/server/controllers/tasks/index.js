ac9e6ef28e549625435e615993668f4ae2ed756f
Heron Eto <41599309+heroneto@users.noreply.github.com>
2020-07-30 18:13:31 -0300

    created index files

--- src/server/controllers/tasks/index.js
+++ src/server/controllers/tasks/index.js
@@ -2,6 +2,8 @@ module.exports = {
   createTask: require('./taskController').createTask,
   deleteTask: require('./taskController').deleteTask,
   getTask: require('./taskController').getTask,
-  updateTask: require('./taskController').updateTask
+  updateTask: require('./taskController').updateTask,
+  searchTask: require('./taskController').searchTasks,
+  getAllTasks: require('./taskController').getAllTasks
 }
 
