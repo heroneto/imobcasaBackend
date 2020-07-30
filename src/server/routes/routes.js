4d4366074b960a7c889777d89fb675a73df38858
Heron Eto <41599309+heroneto@users.noreply.github.com>
2020-07-30 18:12:02 -0300

    feat: added lead routes

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var router = express.Router()
-const {createUser, getAllUsers, updateUser, deleteUser} = require('../controllers').user
-const {searchUser} = require('../controllers').search
-const { createTask, getTask, updateTask, deleteTask } = require('../controllers').tasks
-const {checkAuthentication, userAuthentication} = require('../middlewares').auth
+const { createUser, getAllUsers, updateUser, deleteUser } = require('../controllers').user
+const { searchUser } = require('../controllers').search
+const { searchLeads, getLead, getAllLeads, createLead, updateLead, deleteLead } = require('../controllers').leads
+const { createTask, getTask, updateTask, deleteTask, searchTask, getAllTasks } = require('../controllers').tasks
+const { checkAuthentication, userAuthentication} = require('../middlewares').auth
+const { checkAdminPrivileges } = require('../middlewares').admin
 
 router.get('/', (req,res) =>{
   res.send('Hello world!')
@@ -16,26 +18,57 @@ router.get('/csrf-token', (req, res) => {
 router.route('/login')
   .post(userAuthentication)
 
-router.route('/search/user')
+
+//User routes
+router.route('/user/search')
   .all(checkAuthentication)
   .get(searchUser)
 
 router.route('/user')
   .all(checkAuthentication)
-  .get(getAllUsers)  
+  .all(checkAdminPrivileges)  
   .post(createUser)
   .put(updateUser)
   .delete(deleteUser)
 
+router.route('/users')
+  .all(checkAuthentication)
+  .all(checkAdminPrivileges)
+  .get(getAllUsers)  
+
+
+//Lead routes
 router.route('/lead')
   .all(checkAuthentication)
+  .get(getLead)
+  .post(createLead)
+  .put(updateLead)
+  .delete(deleteLead)
+
+router.route('/leads')
+  .all(checkAuthentication)
+  .all(checkAdminPrivileges)
+  .get(getAllLeads) 
+
+router.route('/lead/search')
+  .all(checkAuthentication)
+  .get(searchLeads)
 
 
+//Task routes
 router.route('/task')
   .all(checkAuthentication)
   .get(getTask)
   .post(createTask)
   .put(updateTask)
   .delete(deleteTask)
+
+router.route('/task/search')
+  .all(checkAuthentication)
+  .get(searchTask)
+
+router.route('/tasks')
+  .all(checkAuthentication)
+  .get(getAllTasks)
 
 module.exports = router
