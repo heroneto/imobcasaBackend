59b6e763893fa400cc09599396c9e1dccd435502
Heron Eto <41599309+heroneto@users.noreply.github.com>
2020-11-28 15:20:08 -0300

    Copied protocols and errors to Helpers folder

---
+++ src/server/helpers/Errors/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  missingParamError: require('./missing-param-errors'),
+  invalidParamError: require('./invalid-param-errors'),
+  serverError: require('./server-error'),
+  noResultsError: require('./no-result-errors'),
+  missingBodyContent: require('./missing-body-errors')
+}
