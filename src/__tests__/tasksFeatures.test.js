4df256c564a2b8522208eafbf73bf2123b1a465e
Heron Eto <41599309+heroneto@users.noreply.github.com>
2020-12-14 21:50:33 -0300

    test: Should return 400 if invalid userid has been provided

--- src/__tests__/tasksFeatures.test.js
+++ src/__tests__/tasksFeatures.test.js
@@ -382,6 +382,21 @@ describe("TASKS FEATURES Tests", () => {
       expect(res.status).toHaveBeenCalledWith(400)
       expect(res.json).toHaveBeenCalledWith(error)
     })
+    test(`Should return 400 if invalid userid has been provided`, async () => {
+      const locals = {
+        reqUserId: adminUser.id,
+        admin: adminUser.admin
+      }
+      const body = mocks.mockTask(adminUser.id, "invalid leadid", taskType.id)
+      body.id = task.id
+      body.resolutionDate = new Date().toISOString()
+      const req = mocks.mockReq(body, null, null, locals)
+      const res = mocks.mockRes()
+      await taskController._update(req, res)
+      const { error } = invalidParamError('leadid')
+      expect(res.status).toHaveBeenCalledWith(400)
+      expect(res.json).toHaveBeenCalledWith(error)
+    })
   })
 
   describe("DELETE ONE Tests", () => {
