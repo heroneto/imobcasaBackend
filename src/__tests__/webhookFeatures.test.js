41527509c7c3ee5fb296eb210df7e0583e56cbc6
Heron Eto <41599309+heroneto@users.noreply.github.com>
2021-01-16 10:28:01 -0300

    test: Should call Next if X-Hub-Signature has been provided

--- src/__tests__/webhookFeatures.test.js
+++ src/__tests__/webhookFeatures.test.js
@@ -34,5 +34,18 @@ describe("WEBHOOK Middlware Tests", () => {
       expect(res.status).toHaveBeenCalledWith(400)
       expect(res.json).toHaveBeenCalledWith(error)
     })
+
+    test("Should call Next if X-Hub-Signature has been provided", async() => {
+      const res = mocks.mockRes()     
+      const xhubsignature = mocks.mockXHubSignature(mocks.mockLeadWebhook())
+      const header = {
+        ['x-hub-signature']: xhubsignature
+      }
+      const req = mocks.mockReq(mocks.mockLeadWebhook(), null, null, null, header)
+      const next = mocks.mockNext()
+      await webhookMiddleware.checkSignature(req, res, next)
+      expect(res.status).not.toHaveBeenCalledWith(400)
+      expect(next).toHaveBeenCalled()
+    })
   })
 })
