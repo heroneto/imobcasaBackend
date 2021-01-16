7618f2841798a5c79c1cbcc461eaefb0c010aa1c
Heron Eto <41599309+heroneto@users.noreply.github.com>
2021-01-16 10:48:38 -0300

    refactor: moved create checksum logic to separate method

--- src/services/WebhookService/index.js
+++ src/services/WebhookService/index.js
@@ -11,19 +11,22 @@ class WebhookService  extends Service{
 
 
 
-  async _createDigest(body){
+  _createDigest(body){
     const payload = JSON.stringify(body)
     const hmac = crypto.createHmac('sha1', this._appSecretKey)
     return Buffer.from('sha1=' + hmac.update(payload).digest('hex'), 'utf8')
   }
 
+  _createChecksum(xHubSignature){
+    return Buffer.from(xHubSignature, 'utf8')
+  }
 
 
   async checkSignature(headers, body){
     await this._checkRequiredFields(this._headersRequiredFields, headers)
     await this._checkBodyExists(body)
-    const digest = await this._createDigest(body)
-    const checksum = Buffer.from(headers['x-hub-signature'], 'utf8')
+    const digest = this._createDigest(body)
+    const checksum = this._createChecksum(headers['x-hub-signature'])
     if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
       await this._throwInvalidParamError("x-hub-signature")
     }
