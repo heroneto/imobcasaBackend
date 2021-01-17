5b7203c63f72ee05c4c4c5cf2fe3975960ce1399
Heron Eto <41599309+heroneto@users.noreply.github.com>
2021-01-17 14:45:29 -0300

    feat: Add return Token Model data

--- src/services/TokenService/index.js
+++ src/services/TokenService/index.js
@@ -1,19 +1,24 @@
 const Service = require('../Service')
 const { checkToken } = require('../apis')
+const { TokenRepository } = require('../../repositories')
 
 class TokenService extends Service {
   _requiredFields = ["accessToken"]
 
   constructor() {
     super()
+    this._tokenRepository = new TokenRepository()
   }
 
   async setToken(fields) {
     this._checkRequiredFields(this._requiredFields, fields)
     const { accessToken } = fields
     const {data} = await checkToken(accessToken)
-    console.log(data)
-    return true
+    const tokenData = {
+      fb_marketing_token: data.access_token
+    }
+    
+    return await this._tokenRepository.setToken(tokenData)
   }
 }
 
