8eded47dc9fda6374296594c3dbaec6f22dc33ac
Heron Eto <41599309+heroneto@users.noreply.github.com>
2021-01-17 15:46:25 -0300

    test: add getToken tests

--- src/__tests__/tokenFeatures.test.js
+++ src/__tests__/tokenFeatures.test.js
@@ -7,17 +7,28 @@ const mocks = new Mocks()
 const modelsExpected = new ModelsExpected
 const databaseSetup = require('../database')
 const { Token } = require('../models')
+const noContentError = require('../helpers/Errors/no-content-error')
+
+
+
+async function creteMockedToken(){
+  const tokenData = {
+    fb_marketing_token: mocks.mockFBMarketingToken()
+  }
+  
+  return await Token.create(tokenData)
+}
 
 describe("TOKEN Controller Tests", () => {
   describe("SET TOKEN Tests", () => {
 
-
     beforeAll(async () => {
       await databaseSetup()
+      await Token.destroy({where: {}})
     })
 
     afterAll(async () => {
-      await Token.destroy({})      
+      await Token.destroy({where: {}})
     })
 
     test("Should return 400 if no token field has been provided", async() => {
@@ -47,5 +58,40 @@ describe("TOKEN Controller Tests", () => {
       expect(res.status).toHaveBeenCalledWith(200)
       expect(res.json).toHaveBeenCalledWith(expect.objectContaining(modelsExpected.tokenExpected()))
     })
+  })
+
+  describe("GET TOKEN Tests", () => {
+    test("Should return 204 if token has not be setted", async () => {
+      const res = mocks.mockRes()
+      const req = mocks.mockReq()
+      await tokensController.getTokens(req, res)
+      const { error } = noContentError("accessToken")
+      expect(res.status).toHaveBeenCalledWith(204)
+      expect(res.json).toHaveBeenCalledWith(error)
+    })
+    test("Should return 200 if token is created", async () => {
+      await creteMockedToken()
+      const res = mocks.mockRes()
+      const req = mocks.mockReq()
+      await tokensController.getTokens(req, res)
+      expect(res.status).toHaveBeenCalledWith(200)
+      expect(res.json).toHaveBeenCalledWith(expect.arrayContaining([expect.objectContaining(modelsExpected.tokenExpected())]))
+    })
+
+    // let token
+    // beforeAll(async () => {
+    //   const tokenData = {
+    //     fb_marketing_token: mocks.mockFBMarketingToken()
+    //   }
+    //   token = await Token.create(tokenData)
+    // })
+
+    // afterAll(async () => {
+    //   Token.destroy({where: {}})
+    // })
+
+    
+
+
   })
 })
