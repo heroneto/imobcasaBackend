279cb2d41b5f4a7c65527b736a134973db825ce3
Heron Eto <41599309+heroneto@users.noreply.github.com>
2021-01-19 17:43:35 -0300

    feat: Should return 400 if token already be set

--- src/services/TokenService/index.js
+++ src/services/TokenService/index.js
@@ -11,12 +11,16 @@ class TokenService extends Service {
   }
 
   async setToken(fields) {
+    const result = await this._tokenRepository.getTokens()
+    if(result.length > 0){
+      this._throwConflictError("token already be set, please update existing token")
+    }    
     this._checkRequiredFields(this._requiredFields, fields)
-    const { accessToken } = fields
+    const { accessToken } = fields    
     const {data} = await checkToken(accessToken)      
     const tokenData = {
       fb_marketing_token: data.access_token
-    }    
+    }  
     return await this._tokenRepository.setToken(tokenData)
   }
 
